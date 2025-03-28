631a69deaaf341678b48e196244303e5e5480c5c
Snehakandikonda25 <snehakandikonda25@gmail.com>
2025-03-28 12:14:32 +0530

    Update RN If.py

--- WEEK2/RN If.py
+++ WEEK2/RN If.py
@@ -1,11 +1,14 @@
 import random
+count=5
 n=random.randint(1,100) 
-while (True):
-   x=int(input("please enter the number:")) 
-   if x < n:
+while (count>0):
+   x=int(input(" please enter the number:")) 
+   if (x < n):
       print("Too low")
    elif x > n :
      print("Too high") 
    else:
-    print("you got it") 
-    break;  
+    print("you won") 
+    break;       
+    count=-1 
+    print("you lost")
